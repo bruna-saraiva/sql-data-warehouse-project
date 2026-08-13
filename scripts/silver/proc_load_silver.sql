697334a64b9fb319a07a8f1c7cc84c45c8ff159f
Bruna Saraiva <102065727+bruna-saraiva@users.noreply.github.com>
2026-08-13 20:05:00 -0300

    improve silver proc load

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -20,31 +20,34 @@ BEGIN
 		TRUNCATE TABLE silver.crm_cust_info;
 		PRINT '>> Inserting Data Into: silver.crm_cust_info';
 		INSERT INTO silver.crm_cust_info(
-		cst_id,
-		cst_key,
-		cst_firstname,
-		cst_lastname,
-		cst_marital_status,
-		cst_gndr,
-		cst_create_date)
-		SELECT cst_id,
-		cst_key,
-		TRIM(cst_firstname) AS cst_firstname,
-		TRIM(cst_lastname) AS cst_lastname,
-		CASE WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
-		WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Married'
-		ELSE 'n/a'
-		END AS cst_marital_status,
-		CASE WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
-		WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
-		ELSE 'n/a'
-		END AS cst_gndr,
-		cst_create_date
+			cst_id,
+			cst_key,
+			cst_firstname,
+			cst_lastname,
+			cst_marital_status,
+			cst_gndr,
+			cst_create_date)
+		SELECT 
+			cst_id,
+			cst_key,
+			TRIM(cst_firstname) AS cst_firstname,
+			TRIM(cst_lastname) AS cst_lastname,
+			CASE WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
+			WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Married'
+			ELSE 'n/a'
+			END AS cst_marital_status,
+			CASE WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
+			WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
+			ELSE 'n/a'
+			END AS cst_gndr,
+			cst_create_date
 		FROM (
-		SELECT *,
-		ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) as flag_last
-		FROM bronze.crm_cust_info
-		)t WHERE flag_last = 1;
+			SELECT *,
+			ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) as flag_last
+			FROM bronze.crm_cust_info
+			WHERE cst_id IS NOT NULL
+			)t
+		 WHERE flag_last = 1;
 
 		SET @end_time = GETDATE();
 		PRINT '>> LOAD Duration: ' + CAST(DATEDIFF(second,@start_time, @end_time) AS NVARCHAR) + ' seconds';
@@ -101,27 +104,27 @@ BEGIN
 			sls_price
 		)
 		SELECT 
-		sls_ord_num,
-		sls_prd_key,
-		sls_cust_id,
-		CASE WHEN sls_order_dt = 0 or LEN(sls_order_dt) != 8 THEN NULL
-			ELSE CAST(CAST(sls_order_dt AS VARCHAR) AS DATE)
-		END AS sls_order_dt,
-		CASE WHEN sls_ship_dt = 0 or LEN(sls_ship_dt) != 8 THEN NULL
-			ELSE CAST(CAST(sls_ship_dt AS VARCHAR) AS DATE)
-		END AS sls_ship_dt,
-		CASE WHEN sls_due_dt = 0 or LEN(sls_due_dt) != 8 THEN NULL
-			ELSE CAST(CAST(sls_due_dt AS VARCHAR) AS DATE)
-		END AS sls_due_dt,
-		CASE WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity * ABS(sls_price)
-			THEN sls_quantity * ABS(sls_price)
-		   ELSE sls_sales
-		END AS sls_sales,
-		sls_quantity,
-		CASE WHEN sls_price IS NULL OR sls_price <= 0
-			THEN sls_sales/ NULLIF(sls_quantity,0)
-			ELSE sls_price
-		END AS sls_price
+			sls_ord_num,
+			sls_prd_key,
+			sls_cust_id,
+			CASE WHEN sls_order_dt = 0 or LEN(sls_order_dt) != 8 THEN NULL
+				ELSE CAST(CAST(sls_order_dt AS VARCHAR) AS DATE)
+			END AS sls_order_dt,
+			CASE WHEN sls_ship_dt = 0 or LEN(sls_ship_dt) != 8 THEN NULL
+				ELSE CAST(CAST(sls_ship_dt AS VARCHAR) AS DATE)
+			END AS sls_ship_dt,
+			CASE WHEN sls_due_dt = 0 or LEN(sls_due_dt) != 8 THEN NULL
+				ELSE CAST(CAST(sls_due_dt AS VARCHAR) AS DATE)
+			END AS sls_due_dt,
+			CASE WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity * ABS(sls_price)
+				THEN sls_quantity * ABS(sls_price)
+			ELSE sls_sales
+			END AS sls_sales,
+			sls_quantity,
+			CASE WHEN sls_price IS NULL OR sls_price <= 0
+				THEN sls_sales/ NULLIF(sls_quantity,0)
+				ELSE sls_price
+			END AS sls_price
 		FROM bronze.crm_sales_details;
 
 		SET @end_time = GETDATE();
